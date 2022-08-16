39dca432458996c2caf5936bc53451cfb14e914d
Gabriel Martins T. Piagentini <gabriel.piagentini@warren.com.br>
2022-08-16 12:38:56 -0300

    Constructor included on CostumerBase

--- CostumersAPI/Costumer/CostumerBase.cs
+++ CostumersAPI/Costumer/CostumerBase.cs
@@ -4,6 +4,23 @@ namespace CostumersAPI.Costumer
 {
     public class CostumerBase
     {
+        public CostumerBase(string fullName, string email, string cpf, string cellphone, DateTime birthdate, bool emailSms, bool whatsapp, string country, string city, string postalCode, string address, int number, string emailConfirmation)
+        {
+            FullName = fullName;
+            Email = email;
+            Cpf = cpf;
+            Cellphone = cellphone;
+            Birthdate = birthdate;
+            EmailSms = emailSms;
+            Whatsapp = whatsapp;
+            Country = country;
+            City = city;
+            PostalCode = postalCode;
+            Address = address;
+            Number = number;
+            EmailConfirmation = emailConfirmation;
+        }
+
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Cpf { get; set; }
