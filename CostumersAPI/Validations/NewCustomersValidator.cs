067af891770417304a564b733e3ea7a9d0c31926
Gabriel Martins T. Piagentini <gabriel.piagentini@warren.com.br>
2022-08-16 13:09:38 -0300

    Validation fields refactor

--- CostumersAPI/Validations/NewCustomersValidator.cs
+++ CostumersAPI/Validations/NewCustomersValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using CostumersAPI.Costumer;
+using FluentValidation.Validators;
 
 namespace CostumersAPI.Validations
 {
@@ -7,31 +8,46 @@ namespace CostumersAPI.Validations
     {
         public NewCustomersValidator()
         {
-            RuleFor(costumer => costumer.FullName).NotEmpty().WithMessage("Favor especificar o campo \"Nome completo\"");
-            RuleFor(costumer => costumer.Email).NotEmpty().WithMessage("Favor especificar o campo \"Email\"");
-            RuleFor(costumer => costumer.Email).EmailAddress().WithMessage("Favor informar um Email válido");
-            RuleFor(costumer => costumer.Cpf).NotEmpty().WithMessage("Favor especificar o campo \"Cpf\"");
-            RuleFor(costumer => costumer.Cellphone).NotEmpty().WithMessage("Favor especificar o campo \"Celular\"");
-            RuleFor(costumer => costumer.Birthdate).NotEmpty().WithMessage("Favor especificar o campo \"Data de Nascimento\"");
-            RuleFor(costumer => costumer.EmailSms).NotEmpty().WithMessage("Favor especificar o campo \"Email e SMS\"");
-            RuleFor(costumer => costumer.Whatsapp).NotEmpty().WithMessage("Favor especificar o campo \"Whatsapp\"");
-            RuleFor(costumer => costumer.Country).NotEmpty().WithMessage("Favor especificar o campo \"País\"");
-            RuleFor(costumer => costumer.City).NotEmpty().WithMessage("Favor especificar o campo \"Cidade\"");
-            RuleFor(costumer => costumer.PostalCode).NotEmpty().WithMessage("Favor especificar o campo \"Cep\"");
-            RuleFor(costumer => costumer.Address).NotEmpty().WithMessage("Favor especificar o campo \"Endereço\"");
-            RuleFor(costumer => costumer.Number).NotEmpty().WithMessage("Favor especificar o campo \"Número\"");
-            RuleFor(costumer => costumer.EmailConfirmation).NotEmpty().WithMessage("Favor especificar o campo \"Confirmar Email\"");
-            RuleFor(costumer => new { costumer.Email, costumer.EmailConfirmation })
-                .Must(costumerEmail => _validateEmailConfirmation(costumerEmail.Email, costumerEmail.EmailConfirmation))
+            RuleFor(costumer => costumer.FullName)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Nome completo\"");
+            RuleFor(costumer => costumer.Email)
+                .NotEmpty()
+                .EmailAddress(EmailValidationMode.Net4xRegex)
+                .WithMessage("Favor especificar o campo \"Email\" válido");
+            RuleFor(costumer => costumer.Cpf)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Cpf\"");
+            RuleFor(costumer => costumer.Cellphone)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Celular\"");
+            RuleFor(costumer => costumer.Birthdate)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Data de Nascimento\"");
+            RuleFor(costumer => costumer.EmailSms)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Email e SMS\"");
+            RuleFor(costumer => costumer.Whatsapp)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Whatsapp\"");
+            RuleFor(costumer => costumer.Country)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"País\"");
+            RuleFor(costumer => costumer.City)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Cidade\"");
+            RuleFor(costumer => costumer.PostalCode)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Cep\"");
+            RuleFor(costumer => costumer.Address)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Endereço\"");
+            RuleFor(costumer => costumer.Number)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Número\"");
+            RuleFor(costumer => costumer)
+                .Must(costumer => costumer.EmailConfirmation.Equals(costumer.Email))
                 .WithMessage("Confirmação de Email divergente");
-
-        }
-
-        private bool _validateEmailConfirmation(string email, string emailConfirmation)
-        {
-            if (email.Equals(emailConfirmation))
-                return true;
-            return false;
         }
     }
 }
--- CostumersAPI/Validations/PutCustomerValidation.cs
+++ CostumersAPI/Validations/PutCustomerValidation.cs
@@ -1,25 +1,50 @@
 ﻿using CostumersAPI.Costumer;
 using FluentValidation;
+using FluentValidation.Validators;
 
 namespace CostumersAPI.Validations
 {
-    public class PutCustomerValidation: AbstractValidator<CostumerBase>
+    public class PutCustomerValidation : AbstractValidator<CostumerBase>
     {
         public PutCustomerValidation()
         {
-            RuleFor(costumer => costumer.FullName).NotEmpty().WithMessage("Favor especificar o campo \"Nome completo\"");
-            RuleFor(costumer => costumer.Email).NotEmpty().WithMessage("Favor especificar o campo \"Email\"");
-            RuleFor(costumer => costumer.Email).EmailAddress().WithMessage("Favor informar um Email válido");
-            RuleFor(costumer => costumer.Cpf).NotEmpty().WithMessage("Favor especificar o campo \"Cpf\"");
-            RuleFor(costumer => costumer.Cellphone).NotEmpty().WithMessage("Favor especificar o campo \"Celular\"");
-            RuleFor(costumer => costumer.Birthdate).NotEmpty().WithMessage("Favor especificar o campo \"Data de Nascimento\"");
-            RuleFor(costumer => costumer.EmailSms).NotEmpty().WithMessage("Favor especificar o campo \"Email e SMS\"");
-            RuleFor(costumer => costumer.Whatsapp).NotEmpty().WithMessage("Favor especificar o campo \"Whatsapp\"");
-            RuleFor(costumer => costumer.Country).NotEmpty().WithMessage("Favor especificar o campo \"País\"");
-            RuleFor(costumer => costumer.City).NotEmpty().WithMessage("Favor especificar o campo \"Cidade\"");
-            RuleFor(costumer => costumer.PostalCode).NotEmpty().WithMessage("Favor especificar o campo \"Cep\"");
-            RuleFor(costumer => costumer.Address).NotEmpty().WithMessage("Favor especificar o campo \"Endereço\"");
-            RuleFor(costumer => costumer.Number).NotEmpty().WithMessage("Favor especificar o campo \"Número\"");
+            RuleFor(costumer => costumer.FullName)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Nome completo\"");
+            RuleFor(costumer => costumer.Email)
+                .NotEmpty()
+                .EmailAddress(EmailValidationMode.Net4xRegex)
+                .WithMessage("Favor especificar o campo \"Email\" válido");
+            RuleFor(costumer => costumer.Cpf)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Cpf\"");
+            RuleFor(costumer => costumer.Cellphone)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Celular\"");
+            RuleFor(costumer => costumer.Birthdate)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Data de Nascimento\"");
+            RuleFor(costumer => costumer.EmailSms)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Email e SMS\"");
+            RuleFor(costumer => costumer.Whatsapp)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Whatsapp\"");
+            RuleFor(costumer => costumer.Country)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"País\"");
+            RuleFor(costumer => costumer.City)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Cidade\"");
+            RuleFor(costumer => costumer.PostalCode)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Cep\"");
+            RuleFor(costumer => costumer.Address)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Endereço\"");
+            RuleFor(costumer => costumer.Number)
+                .NotEmpty()
+                .WithMessage("Favor especificar o campo \"Número\"");
         }
     }
 }
