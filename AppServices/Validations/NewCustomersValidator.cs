34901188acf245e5451c8ce0aef5d9f4c378b95e
Gabriel Martins T. Piagentini <gabriel.piagentini@warren.com.br>
2022-08-25 10:05:02 -0300

    Code design approvements

--- AppServices/Validations/NewCustomersValidator.cs
+++ AppServices/Validations/NewCustomersValidator.cs
@@ -11,37 +11,47 @@ namespace AppServices.Validations
         {
             RuleFor(costumer => costumer.FullName)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Nome completo\"");
+
             RuleFor(costumer => costumer.Email)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Email\"")
                 .EmailAddress(EmailValidationMode.Net4xRegex).WithMessage("Favor especificar em e-mail válido");
+
             RuleFor(costumer => costumer.Cpf)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Cpf\"")
                 .Must(cpf => cpf.IsValidCPF()).WithMessage("Cpf especificado não é válido");
+
             RuleFor(costumer => costumer.Cellphone)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Celular\"")
                 .Must(cellphone => cellphone.IsValidCellphone()).WithMessage("Celular informado é inválido");
+
             RuleFor(costumer => costumer.Birthdate)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Data de Nascimento\"")
                 .Must(birthdate => birthdate.GetAge() >= 18).WithMessage("Necessário ter mais de 18 anos");
+
             RuleFor(costumer => costumer.EmailSms)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Email e SMS\"");
+
             RuleFor(costumer => costumer.Whatsapp)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Whatsapp\"");
+
             RuleFor(costumer => costumer.Country)
                 .NotEmpty().WithMessage("Favor especificar o campo \"País\"");
+
             RuleFor(costumer => costumer.City)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Cidade\"");
+
             RuleFor(costumer => costumer.PostalCode)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Cep\"")
                 .Must(postalCode => postalCode.IsValidPostalCode()).WithMessage("Cep informado é inválido");
+
             RuleFor(costumer => costumer.Address)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Endereço\"");
+
             RuleFor(costumer => costumer.Number)
                 .NotEmpty().WithMessage("Favor especificar o campo \"Número\"");
+
             RuleFor(costumer => costumer)
             .Must(costumer => costumer.EmailConfirmation.Equals(costumer.Email)).WithMessage("Confirmação de Email divergente");
-
-
         }
     }
 }
